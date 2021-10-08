98b92ae783636d012cec0e34dab8ad255376806d
Øystein Kolsrud <yko@qlik.com>
2021-10-08 12:41:03 +0200

    Added functionality for scaning specific app.

--- src/QlikChartReorienter/Program.cs
+++ src/QlikChartReorienter/Program.cs
@@ -12,6 +12,9 @@ namespace QlikChartReorienter
 	class Program
     {
         private static StreamWriter _logStreamWriter;
+        private static bool _commitChanges = false;
+        private static bool _scanAllApps = false;
+        private static string _scanSpecificAppId = null;
 
         private static void Write(string message)
         {
@@ -23,25 +26,81 @@ namespace QlikChartReorienter
         {
             Write(message + Environment.NewLine);
         }
-		
+
+
+        private static void ParseArgs(string[] args)
+        {
+            if (args.Length == 0)
+                PrintUsage();
+
+            _commitChanges = args.Contains("--commit");
+            _scanAllApps = args.Contains("--all");
+            _scanSpecificAppId = GetArg(args, "--app");
+            if (!_scanAllApps && (_scanSpecificAppId == null))
+            {
+                Error("Exactly one of the flags --all and --app must be defined.");
+            }
+            if (_scanAllApps && (_scanSpecificAppId != null))
+            {
+                Error("Illegal combination of flags. The flags --all and --app are mutually exclusive.");
+            }
+        }
+
+        private static void Error(string msg)
+        {
+            Console.WriteLine("--------------------------------------------------------------------------");
+            Console.WriteLine("--- Error: " + msg);
+            Console.WriteLine("--------------------------------------------------------------------------");
+            PrintUsage();
+        }
+
+        private static string GetArg(string[] args, string arg)
+        {
+            var i = Array.IndexOf(args, arg);
+            if (i == -1)
+                return null;
+
+            if (i == arg.Length - 1)
+            {
+                Error("No argument found for app flag.");
+            }
+
+            var result = args[i + 1];
+            if (!Guid.TryParse(result, out _))
+            {
+                Error($"Unable to parse argument \"{args[i+1]}\" as Guid.");
+            }
+
+            return result;
+        }
+
+        static void PrintUsage()
+        {
+            Console.WriteLine("Usage:   " + System.AppDomain.CurrentDomain.FriendlyName + " (--all | --app <appId>) [--commit]");
+            Console.WriteLine("Example: " + System.AppDomain.CurrentDomain.FriendlyName + " --all --commit");
+            Console.WriteLine("         " + System.AppDomain.CurrentDomain.FriendlyName + " --app 9183fa90-69f6-4864-862e-d6ff75865fb0 --commit");
+            Environment.Exit(0);
+        }
+
         static void Main(string[] args)
         {
+            ParseArgs(args);
+
             var now = DateTime.Now;
             var logFileName = "qlik-chart-reorient-log-" + now.ToString("yyyy-MM-ddTHH-mm-ss") + ".log";
             Console.WriteLine("Writing to log file: " + logFileName);
             _logStreamWriter = new StreamWriter(logFileName,false);
-            var commitChanges = false;
-            if (args.Length > 0)
-                commitChanges = args.First() == "commit";
 
-            if (!commitChanges)
+            if (!_commitChanges)
                 WriteLine("Dry run only. No changes will be committed.");
 
             var location = Location.FromUri(@"https:\\localhost");
             var certs = CertificateManager.LoadCertificateFromStore();
 			location.AsDirectConnection("INTERNAL", "sa_api", certs, false);
 
-            var appIds = location.GetAppIdentifiers().ToArray();
+            AppIdentifier[] appIds;
+            appIds = _scanAllApps ? location.GetAppIdentifiers().OfType<AppIdentifier>().ToArray() : new []{new AppIdentifier{AppId=_scanSpecificAppId}};
+
             WriteLine($"Scanning {appIds.Length} apps...");
 
             var reoriented = new List<IAppIdentifier>();
@@ -49,8 +108,8 @@ namespace QlikChartReorienter
             foreach (var appIdentifier in appIds)
             {
                 n++;
-                WriteLine($"({n}/{appIds.Length}) - Scanning \"{appIdentifier.AppName}\" ({appIdentifier.AppId})");
-                if (ReorientForApp(location, appIdentifier, commitChanges))
+                WriteLine($"({n}/{appIds.Length}) - Scanning \"{appIdentifier.AppName ?? "<App name unknown>"}\" ({appIdentifier.AppId})");
+                if (ReorientForApp(location, appIdentifier, _commitChanges))
                     reoriented.Add(appIdentifier);
             }
 
@@ -60,7 +119,7 @@ namespace QlikChartReorienter
                 WriteLine($"\t{appIdentifier.AppName} ({appIdentifier.AppId})");
             }
 
-            if (commitChanges)
+            if (_commitChanges)
             {
                 WriteLine("*** Changes applied! ***");
             }
@@ -70,12 +129,17 @@ namespace QlikChartReorienter
             }
         }
 
-        private static bool ReorientForApp(ILocation location, IAppIdentifier appIdentifier, bool commitChanges)
+        private static bool ReorientForApp(ILocation location, AppIdentifier appIdentifier, bool commitChanges)
         {
             try
             {
                 using (var app = location.App(appIdentifier, session: Session.Random, noData: true))
                 {
+                    if (appIdentifier.AppName == null)
+                    {
+                        appIdentifier.AppName = app.GetAppProperties().Title;
+                    }
+
                     var infos = app.GetAllInfos();
                     var combocharts = infos.Where(i => i.Type == "combochart").ToArray();
                     WriteLine($"\t  |- Found {combocharts.Length} combo charts");
@@ -90,7 +154,7 @@ namespace QlikChartReorienter
                         var p = o.GetProperties().As<CombochartProperties>();
                         if (p.Orientation == Orientation.Horizontal)
                         {
-                            WriteLine(
+                            Write(
                                 $"\t  |- Changing {(string.IsNullOrEmpty(extendsId) ? "chart" : "master object")} to vertical: " +
                                 p.Info.Id);
                             p.Orientation = Orientation.Vertical;
@@ -98,17 +162,19 @@ namespace QlikChartReorienter
                             {
                                 if (commitChanges)
                                 {
+                                    WriteLine("");
                                     o.SetProperties(p);
                                 }
                                 else
                                 {  
-                                    WriteLine($"\t  |- Dry run only. No changes applied.");
+                                    WriteLine($" (Dry run only. No changes applied.)");
                                 }
 
                                 modified++;
                             }
                             catch
                             {
+                                WriteLine("");
                                 WriteLine("\t  |- Failed to set properties.");
                             }
                         }
